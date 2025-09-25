23f504f3f826e3d0b3a9406b27efb352a4e2d287
marmoszy <89835375+marmoszy@users.noreply.github.com>
2025-09-25 22:59:10 +0200

    Add files via upload

---
+++ pmi/pmi_art.js
@@ -0,0 +1,126 @@
+var art=`
+<div class="accordion" id="accordionPanelsStayOpenExample">
+
+<!----------- Article 4 ----------------->
+<!--
+	<div class="accordion-item">
+		<h2 class="accordion-header">
+			<button data-bs-target="#a__4" class="accordion-button collapsed4" type="button" data-bs-toggle="collapse"
+				aria-expanded="false">
+1. Tytuł artykułu 4
+			<span style="color:gray;font-size:small;margin-top:0;margin-left:24px">
+2. Data 4
+			</span>
+			</button>
+		</h2>
+		<div class="accordion-body">
+3. Pierwsza część artykułu 4
+		</div>
+		<div id="a__4" class="accordion-collapse collapse show">
+			<div class="accordion-body">
+4. Druga część artykułu 4
+			</div>
+		</div>
+	</div>
+-->
+
+<!----------- Article 3 ----------------->
+<!--
+	<div class="accordion-item">
+		<h2 class="accordion-header">
+			<button data-bs-target="#a__3" class="accordion-button collapsed3" type="button" data-bs-toggle="collapse"
+				aria-expanded="false">
+1. Tytuł artykułu 3
+			<span style="color:gray;font-size:small;margin-top:0;margin-left:24px">
+2. Data 3
+			</span>
+			</button>
+		</h2>
+		<div class="accordion-body">
+3. Pierwsza część artykułu 3
+		</div>
+		<div id="a__3" class="accordion-collapse collapse show">
+			<div class="accordion-body">
+4. Druga część artykułu 3
+			</div>
+		</div>
+	</div>
+-->
+
+<!----------- Article 2 ----------------->
+
+	<div class="accordion-item">
+		<h2 class="accordion-header">
+			<button data-bs-target="#a__2" class="accordion-button collapsed1" data-bs-toggle="collapse" aria-expanded="true" type="button">
+Wiadomość 2
+			<span style="color:gray;font-size:small;margin-top:0;margin-left:24px">
+26 wrześsnia 2025
+			</span>
+			</button>
+		</h2>
+		<div class="accordion-body">
+Po raz ...
+		</div>
+		<div id="a__2" class="accordion-collapse collapse show">
+			<div class="accordion-body">
+
+Oto ...
+<p>
+
+<center>
+	<a href="pmi_banner.png"><img src="pmi_banner.png" style="max-width:400px;width:100%"></a>
+	<p> Podpis
+</center>
+
+<p>
+W 2025 r. ... 
+
+			</div> 
+		</div>
+	</div>
+
+<!----------- Article 1 ----------------->
+	<div class="accordion-item">
+		<h2 class="accordion-header">
+			<button data-bs-target="#a__1" class="accordion-button collapsed" aria-expanded="false" type="button" data-bs-toggle="collapse">
+Wiadomość 1 
+				<span style="color:gray;font-size:small;margin-top:0;margin-left:24px">
+25 września 2025
+				</span>
+			</button>
+		</h2>
+		<div class="accordion-body">
+Podobnie jak w ubiegłym roku ...
+		</div>
+		<div id="a__1" class="accordion-collapse collapse show0">
+			<div class="accordion-body">
+Poniżej ...
+<center>
+	<a href="pmi_banner.png"><img src="pmi_banner.png" style="max-width:400px;width:100%"></a>
+	<p>Podpis 
+</center>
+<p>
+
+<p>
+A tymczasem 
+
+<p>
+Z pozdrowieniem,
+<br>
+PMI-PL<sub>25</sub>
+<br>
+<br>
+			</div>
+		</div>
+	</div>
+
+<!----------- End of Articles ----------------->
+
+</div>
+	
+<!--<h3><span class="sep"></span></h3>-->
+<br>
+
+`;
+
+
