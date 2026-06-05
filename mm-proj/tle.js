5fe1de6cb25045796c2511474ca5e51f36249124
marmoszy <89835375+marmoszy@users.noreply.github.com>
2026-06-05 11:44:52 +0200

    Add files via upload

---
+++ mm-proj/tle.js
@@ -0,0 +1,228 @@
+gpsops_tle=`GPS BIIR-2  (PRN 13)    
+1 24876U 97035A   25080.48978537  .00000030  00000+0  00000+0 0  9998
+2 24876  55.7700 116.2835 0087610  53.8282 307.0467  2.00562639202878
+GPS BIIR-4  (PRN 20)    
+1 26360U 00025A   25080.87463384 -.00000048  00000+0  00000+0 0  9993
+2 26360  54.8784  37.5932 0037927 231.6813 136.4375  2.00557194182201
+GPS BIIR-5  (PRN 22)    
+1 26407U 00040A   25080.82810833  .00000044  00000+0  00000+0 0  9997
+2 26407  54.9678 233.1364 0134201 298.5466 234.0862  2.00564484180875
+GPS BIIR-8  (PRN 16)    
+1 27663U 03005A   25080.40937794  .00000045  00000+0  00000+0 0  9997
+2 27663  55.0035 232.9633 0143454  49.8891 320.8760  2.00559130162237
+GPS BIIR-11 (PRN 19)    
+1 28190U 04009A   25080.62069478  .00000005  00000+0  00000+0 0  9993
+2 28190  55.2134 293.9262 0101969 157.5196 161.1735  2.00570044153864
+GPS BIIR-13 (PRN 02)    
+1 28474U 04045A   25080.35183656 -.00000082  00000+0  00000+0 0  9999
+2 28474  55.3536 343.1043 0163898 301.4169 241.1137  2.00657900149340
+GPS BIIRM-1 (PRN 17)    
+1 28874U 05038A   25080.89304588  .00000011  00000+0  00000+0 0  9995
+2 28874  55.2579 291.3857 0133284 289.6345 251.9092  2.00566519142769
+GPS BIIRM-2 (PRN 31)    
+1 29486U 06042A   25080.26717258  .00000003  00000+0  00000+0 0  9995
+2 29486  54.6792 169.3076 0104342  45.4292 304.4495  2.00567658135306
+GPS BIIRM-3 (PRN 12)    
+1 29601U 06052A   25079.71086750  .00000045  00000+0  00000+0 0  9995
+2 29601  55.0430 231.9895 0087550  85.2451 275.7253  2.00557806134354
+GPS BIIRM-4 (PRN 15)    
+1 32260U 07047A   25080.51889702  .00000027  00000+0  00000+0 0  9993
+2 32260  53.7714  98.7612 0159100  80.0416 281.8007  2.00551162127738
+GPS BIIRM-5 (PRN 29)    
+1 32384U 07062A   25080.07625137  .00000004  00000+0  00000+0 0  9993
+2 32384  55.4049 292.3234 0029782 153.5314  16.5533  2.00562765126425
+GPS BIIRM-6 (PRN 07)    
+1 32711U 08012A   25080.42934269  .00000003  00000+0  00000+0 0  9990
+2 32711  54.4566 167.9979 0195701 241.9857 116.0630  2.00555938124677
+GPS BIIRM-8 (PRN 05)    
+1 35752U 09043A   25080.92677410 -.00000031  00000+0  00000+0 0  9992
+2 35752  55.9004  45.2694 0054108  74.9449 295.2250  2.00552782114295
+GPS BIIF-1  (PRN 25)    
+1 36585U 10022A   25080.24782766  .00000040  00000+0  00000+0 0  9991
+2 36585  54.3631 226.8157 0121586  63.7824 296.6268  2.00565481108509
+GPS BIIF-3  (PRN 24)    
+1 38833U 12053A   25080.61156237  .00000002  00000+0  00000+0 0  9992
+2 38833  53.5281 162.1290 0163384  59.9597 301.7002  2.00561883 90365
+GPS BIIF-4  (PRN 27)    
+1 39166U 13023A   25080.97270256  .00000018  00000+0  00000+0 0  9994
+2 39166  54.8728 287.3531 0133037  46.9982 314.1396  2.00561781 86804
+GPS BIIF-5  (PRN 30)    
+1 39533U 14008A   25080.97217557  .00000001  00000+0  00000+0 0  9993
+2 39533  53.6077 167.7553 0076863 223.3124 136.1217  2.00558215 81169
+GPS BIIF-6  (PRN 06)    
+1 39741U 14026A   25080.75822161 -.00000083  00000+0  00000+0 0  9994
+2 39741  56.6682 349.1073 0036532 318.9365  40.8238  2.00580405 79465
+GPS BIIF-7  (PRN 09)    
+1 40105U 14045A   25080.83943513  .00000034  00000+0  00000+0 0  9990
+2 40105  55.0685 106.5113 0028035 118.1512 242.1903  2.00555241 77891
+GPS BIIF-8  (PRN 03)    
+1 40294U 14068A   25080.21220241 -.00000027  00000+0  00000+0 0  9995
+2 40294  56.6553  48.4601 0056776  63.6906 296.8848  2.00577520 76124
+GPS BIIF-9  (PRN 26)    
+1 40534U 15013A   25080.35498113  .00000034  00000+0  00000+0 0  9990
+2 40534  53.2608 222.9139 0097672  34.6329 325.9706  2.00548622 72749
+GPS BIIF-10 (PRN 08)    
+1 40730U 15033A   25080.51518117  .00000016  00000+0  00000+0 0  9995
+2 40730  54.3539 285.7081 0104606  24.0633 336.4487  2.00552669 70918
+GPS BIIF-11 (PRN 10)    
+1 41019U 15062A   25081.06933172 -.00000023  00000+0  00000+0 0  9990
+2 41019  56.6334  48.2828 0102986 228.6250 130.4823  2.00566769 68766
+GPS BIIF-12 (PRN 32)    
+1 41328U 16007A   25080.15784067  .00000028  00000+0  00000+0 0  9993
+2 41328  55.2540 107.4420 0087197 241.7271 117.4473  2.00574020 66756
+GPS BIII-1  (PRN 04)    
+1 43873U 18109A   25080.79489182  .00000034  00000+0  00000+0 0  9992
+2 43873  55.4199 110.0527 0033248 197.4543 156.6800  2.00566681 45996
+GPS BIII-2  (PRN 18)    
+1 44506U 19056A   25080.86561512 -.00000084  00000+0  00000+0 0  9990
+2 44506  55.8037 349.0945 0049256 193.7801 113.4448  2.00561061 40996
+GPS BIII-3  (PRN 23)    
+1 45854U 20041A   25080.08995611 -.00000032  00000+0  00000+0 0  9991
+2 45854  56.3481  46.5780 0051323 199.7684 209.7549  2.00568235 34951
+GPS BIII-4  (PRN 14)    
+1 46826U 20078A   25080.81850194  .00000039  00000+0  00000+0 0  9994
+2 46826  54.0873 229.1303 0055657 199.2569 347.0589  2.00558704 32433
+GPS BIII-5  (PRN 11)    
+1 48859U 21054A   25080.81018748 -.00000085  00000+0  00000+0 0  9990
+2 48859  55.3346 350.6193 0018579 230.1188 139.1635  2.00569300 27688
+GPS BIII-6  (PRN 28)    
+1 55268U 23009A   25055.80589204  .00000001  00000+0  00000+0 0  9992
+2 55268  55.0899 167.2640 0000971  39.5557 308.9006  2.00545614 15662
+GPS BIII-7  (PRN 01)    
+1 62339U 24242A   25080.43401963 -.00000087  00000+0  00000+0 0  9996
+2 62339  54.9886 352.5149 0004815 329.0533 247.7113  2.00557116  2179
+`;
+
+
+function jd2000(now) {
+  var year   = now.getUTCFullYear();
+  var month  = now.getUTCMonth() + 1;
+  var day    = now.getUTCDate();
+  var hour   = now.getUTCHours();
+  var minute = now.getUTCMinutes();
+  var second = now.getUTCSeconds();
+
+  if ((month == 1)||(month == 2)) {year -= 1; month += 12;}
+  var a = Math.floor(year/100);
+  var b = 2 - a + Math.floor(a/4);
+  var c = Math.floor(365.25*year);
+  var d = Math.floor(30.6001*(month + 1));
+  return b + c + d - 730550.5 + day + (hour + minute/60.0 + second/3600.0)/24.0;
+}
+function MST(now, lon) {
+  var jd = jd2000(now);
+  var jt = jd/36525.0;     // julian centuries since J2000.0
+  var mst = 280.46061837 + 360.98564736629*jd + 0.000387933*jt*jt 
+          - jt*jt*jt/38710000 + lon; 
+  if (mst > 0.0) while(mst > 360.0) mst -= 360.0;
+  else while(mst < 0.0) mst += 360.0;
+  return mst;
+}
+function read_tle(s,opt) { opt = opt || 0; // opt: 0-tle 1-alm
+  var pi = Math.PI; //console.log("read_tle: "+s);
+  var alm=[], tle=[];
+  var a=6378137,EARTH_RATE = 7.2921151467e-5,rad=pi/180;
+  var ss=s.split("\n");
+  for(var i=0,j=0;i<ss.length;i++) {
+    var prn,id,inc,raan,ecc,per,M0,n0,toa,a_sqr,Omega,Omega_dot,week,y,d,n0_dot,h;
+    if(ss[i].length>2) { var name;
+      if(j==0) {
+        name=ss[i];
+        var nam=name.substring(0,3);
+        h=(nam=="GPS"?1:nam=="COS"?2:nam=="GSA"?4:nam=="BEI"?8:0);
+        var sn=/\(PRN (.*)\)/.exec(name);
+        if(sn) {prn=parseInt(sn[1]);if(!prn) prn=1+(i+2)/3;} else prn=1+(i+2)/3;
+      } else if(j==1) {
+        id=parseInt(ss[i].substring(2,7));
+        y=parseInt(ss[i].substring(18,20)); y+=(y<70)?2000:1900;
+        d=parseFloat(ss[i].substring(20,32));
+        var dt=new Date(Date.UTC(y,0,d)); dt.setSeconds((d*86400)%86400);
+        var gps_epoch=(new Date(Date.UTC(1980,0,6))).getTime()/1000	;
+        toa=dt.getTime()/1000;
+        week=Math.floor((toa-gps_epoch)/86400/7)-1024;
+        toa = toa - gps_epoch - 7*86400*(week+1024);
+        n0_dot=parseFloat(ss[i].substring(33,43));
+      } else if(j==2) {
+        inc=parseFloat(ss[i].substring(8,16))*rad;
+        raan=parseFloat(ss[i].substring(17,25))*rad;
+        ecc=parseFloat('0.'+ss[i].substring(26,33));
+        per=parseFloat(ss[i].substring(34,42))*rad;
+        M0=parseFloat(ss[i].substring(43,51))*rad;
+        n0=parseFloat(ss[i].substring(52,63));
+        a_sqr=Math.pow(398600500000000/Math.pow(n0/86400*2*pi,2),1/6);
+        Omega_dot=-3/2*0.001081874*a*a/
+           Math.pow(a_sqr*a_sqr*(1-ecc*ecc),2)*n0/86400*2*pi*Math.cos(inc);
+        Omega=raan - Omega_dot*toa;
+        alm.push([id,prn,h,ecc,toa,inc,Omega_dot,a_sqr,Omega,per,M0,0,0,week]);
+        tle.push([id,prn,h,ecc,d,inc/rad,Omega_dot,a_sqr,raan/rad,per/rad,
+                  M0/rad,n0,n0_dot,y,name]);
+      }
+      j=(j+1)%3;
+    }
+  }
+  return(opt==0?tle:alm);
+}
+function Rev(n) { var x=n;
+  if(x>0.0) {while(x>360.0)x-=360.0;} else {while(x<0.0)x+=360.0;} 
+  return(x);
+}
+function tle2lla(now, tle) {with(Math){  // now in sec
+  var pi = Math.PI; 
+  var a = 6378135, rad = pi/180, deg = 180/pi;
+  var gmst = MST(new Date(now*1000), 0.0 );
+  var lla=[];
+  for(var i=0;i<tle.length;i++) { 
+    var rev = tle[i][11],    n_dot = tle[i][12],  e = tle[i][3],
+        M0 = tle[i][10]*rad, per = tle[i][9]*rad, raan = tle[i][8]*rad, 
+	    inc = tle[i][5]*rad, year = tle[i][13],   day = tle[i][4];
+    var epoch = new Date(Date.UTC(year,0,day));
+    epoch.setSeconds((day*86400)%86400);
+    var t = (now - epoch.getTime()/1000)/86400; // in days!
+    var T = 24/(rev + n_dot*t);                 // Period in hours
+    var r = 1000*pow(6028.9*60*T, 2/3);         // semi major axis in m
+    M0 +=  2*pi*(rev*t + n_dot*t*t/2);
+    var E0 = M0 + e*sin(M0) +  e*e*sin(2*M0)/2;
+    for(var err=1; abs(err)>0.00000001; E0 -= err) {err = E0 - e*sin(E0) - M0;}
+    // perifocal coordinate system
+    var x0 = r*(cos(E0)-e);          // r*Cos(trueanomaly)
+    var y0 = r*sqrt(1-e*e)*sin(E0);  // r*sin(trueanomaly)
+    var r0 = sqrt(x0*x0+y0*y0);      // distance
+    per += rad*4.97*t*pow(a/r,3.5)*(5*cos(inc)*cos(inc)-1)/((1-e*e)*(1-e*e));
+    raan -= rad*9.95*t*pow(a/r,3.5)*cos(inc)/((1-e*e)*(1-e*e));
+    var Px =  cos(per)*cos(raan)-sin(per)*sin(raan)*cos(inc);
+    var Py =  cos(per)*sin(raan)+sin(per)*cos(raan)*cos(inc);
+    var Pz =  sin(per)*sin(inc);
+    var Qx = -sin(per)*cos(raan)-cos(per)*sin(raan)*cos(inc);
+    var Qy = -sin(per)*sin(raan)+cos(per)*cos(raan)*cos(inc);
+    var Qz =  cos(per)*sin(inc);
+    var x = Px * x0 + Qx * y0;        // xyz ECI coordinate system (geocentric)
+    var y = Py * x0 + Qy * y0;
+    var z = Pz * x0 + Qz * y0;
+    var lat = atan2(z, sqrt(x*x+y*y) );
+    var lon = Rev(atan2(y,x)*deg-gmst)*rad;
+    if(lon>pi) lon-=2*pi;
+    var alt = r0 - a;
+    lla.push([lat,lon,alt, now, epoch.getTime()/1000, t, tle[i][14].trim(), T, tle[i], M0,per,raan ]);  
+  }
+  return(lla);
+}}
+function parseTle(tle,color,t) {
+	var tle = tle || gpsops_tle;
+	var tt = read_tle(tle);
+	var utc = new Date();
+	var LL=[],names=[];
+	for( var i=0;i<tt.length;i++) {
+		LL.push([]);
+	}
+	for(var j=0;j<t.length;j++) {
+		var tsec = utc.getTime()/1000 + t[j];
+		var lla = tle2lla(tsec,tt);
+		var ll=[];
+		for (var i=0;i<lla.length;i++) {
+			LL[i].push([lla[i][1]*180/Math.PI,lla[i][0]*180/Math.PI]);
+			if(j==0) names.push([[lla[i][6]]]);
+		}
+		
+	}
+	return({data:LL, color: color?color:'green', names:names, lla:lla} );
+}
